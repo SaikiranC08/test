632554e26e6070c487e919cea65d8b9a4518e905
SaikiranC08 <schevula26@gmail.com>
2025-09-26 23:48:47 +0530

    Add small error Java file for branch-5

--- TestError.java
+++ TestError.java
@@ -1,1 +1,1 @@
-public class TestError { public static void mmain(String[] args) { int a = 5 int b = 0; Ssystem.out.println(a / b);+ } }
+public class TestError { public staatic void main(String[] args) { int a = 5 int b = 0; Ssystem.out.println(a / b); } }
