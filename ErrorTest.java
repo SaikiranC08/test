cab2209d397092a51899be31be3ac5ed8b30a0ec
SaikiranC08 <schevula26@gmail.com>
2025-10-02 00:24:25 +0530

    Add small error file to test reviewer

--- ErrorTest.java
+++ ErrorTest.java
@@ -1,1 +1,1 @@
-pu dudbl ic Cclass klErrorTest {j ppublic staatic void main(String[] args) { int x = 10/0; } }
+pu kdudbl ic Cclass klErrorTest {j ppublic staatic void main(String[] args) { int x = 10/0; } }
