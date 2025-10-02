2e0121f42cd9e1cbceccf911604338de205b296c
SaikiranC08 <schevula26@gmail.com>
2025-10-02 23:43:18 +0530

    Add small error file to test reviewer

--- ErrorTest.java
+++ ErrorTest.java
@@ -1,1 +1,1 @@
-public staticc Cclass klErrorTest {j ppublic staatic void main(String[] args) { int x = 10/0; } }
+public staticc Class klErrorTest {j ppublic staatic void main(String[] args) { int x = 10/0; } }
