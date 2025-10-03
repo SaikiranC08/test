0ecda75d288c6f3e383017fd87f839021a76e19a
SaikiranC08 <schevula26@gmail.com>
2025-10-03 10:53:54 +0530

    Add small error file to test reviewer

--- ErrorTest.java
+++ ErrorTest.java
@@ -1,1 +1,1 @@
-public staticc Class klErrorTest {j public staatic void main(String[] args) { int x = 10//0} }
+public staticc Class klErrorTest {j public staatic void main(String[] args) { int x = 1=//0} }
